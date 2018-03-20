1b0c2124aa64d0c21ebb809444f3bb4606dfc612
Jojo <jody.sekhon@gmail.com>
2018-03-20 00:48:39 +0000

    Changed program so it accounted for authorizer being false, .env file missing or missing information

--- download_avatars.js
+++ download_avatars.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 var rOwner = process.argv[2];
 var rName = process.argv[3];
 var dir = './avatars';
-
+var token = process.env.GITHUB_TOKEN
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -13,6 +13,8 @@ function getRepoContributors(repoOwner, repoName, cb) {
     console.log("Please provide valid Repo Owner and Repo Name");
     return;
   }
+
+
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
@@ -23,14 +25,26 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
   request(options, function(err, res, body) {
     res.setEncoding('utf8');
-    //This is to check if the avatar directory exists
-    if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
+    if (res.statusCode === 401 || res.statusCode === 404) {
+
+    console.log("This repository/owner does not exist, or authorization failed");
+   return;
+   }
+   else if(token === undefined) {
+    console.log("Please provide .env file or the correct information in the .env file")
+  return;
   }
     var arr = JSON.parse(body);
     arr.forEach(function(user){
-      cb(err, user)
-    });
+
+  //This is to check if the avatar directory exists
+    if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+   }
+
+    cb(err, user)
+
+   });
   });
 }
 
